37a1043bf9a05481c462ae40de2cbf1888316a75
LevinCoder13 <benbenlevin12345@gmail.com>
2024-10-26 16:08:28 +0530

    Update urls.py

--- locus/urls.py
+++ locus/urls.py
@@ -24,7 +24,8 @@ urlpatterns = [
     path('',include('locusnews.urls'))
 ]
 
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+urlpatterns += staticfiles_urlpatterns() # new
 if settings.DEBUG:
     urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
