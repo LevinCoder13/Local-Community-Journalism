3bafbbbaf65f29da1a9366eec4950e281f4feef5
LevinCoder13 <benbenlevin12345@gmail.com>
2024-10-26 16:19:38 +0530

    Update settings.py

--- locus/settings.py
+++ locus/settings.py
@@ -128,6 +128,7 @@ STATIC_ROOT = BASE_DIR / "staticfiles" # new
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL  = '/media/' 
 
+RUNSERVER_IP = '0.0.0.0'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
